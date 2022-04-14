e61d0389e2795f63476cf747e9c4b5f76677bd43
rodrigocamperopaz <rodrigo.campero@trueforce.com>
2022-04-14 08:26:34 -0400

    Initial commit

---
+++ TestDrivenDevelopment/Validators/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TestDrivenDevelopment.Validators
+{
+    public class EmailValidator
+    {
+        const string EmailValidatorRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
+        public bool IsValid(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using TestDrivenDevelopment.Validators;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void TestIsValid()
+        {
+            Assert.Pass();
+        }
+    }
+}
